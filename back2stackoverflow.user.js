3e66f6cc944d2b538fb1a8e7ad8aed39b6fc46c0
Taraflex <al.qw0101@gmail.com>
2019-07-04 05:48:47 +0300

    update version

--- back2stackoverflow.user.js
+++ back2stackoverflow.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         back2stackoverflow
-// @version      0.0.12
+// @version      0.0.13
 // @description  Redirect to stackoverflow.com from machine-translated sites
 // @namespace    taraflex
 // @author       taraflex.red@gmail.com
